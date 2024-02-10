1429d33e447f06e25a176a1237dc6ee381e0c070
hitesh-kumar05 <hiteshofficial67@gmail.com>
2024-02-10 14:08:35 +0530

    add this new content

---
+++ 2.Node-Module-System/app.js
@@ -0,0 +1,3 @@
+var log=require('./create_module');
+
+log('message');
---
+++ 2.Node-Module-System/module.js
@@ -0,0 +1,3 @@
+//module is encapuslate and reusable code of pieace .which can use globally any where.
+//which include id,path exports,filename etc.
+console.log(module);
